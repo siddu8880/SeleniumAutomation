03c3396146acaafb3d4946fb3185793eda33afe6
siddu8880 <siddumengde404@gmail.com>
2021-03-12 11:13:11 +0530

    AutomationDraft Mar12-1

---
+++ WebAutomation/src/com/sgtesting/tests/Assignment2.java
@@ -0,0 +1,147 @@
+package com.sgtesting.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment2 {
+	public static WebDriver oBrowser=null;
+	public static void main(String[] args) {
+		lanuchBrowser();
+		navigate();
+		login();
+		miniPopwindow();
+		createUser();
+		miniPopwindow();
+		//deleteUser();
+		//logout();
+		//closeApplication();
+	}
+public static void lanuchBrowser()
+{
+	try
+	{
+		String path=System.getProperty("user.dir");	
+		System.setProperty("webdriver.chrome.driver", path+"\\Library\\drivers\\chromedriver.exe");
+		oBrowser=new ChromeDriver();
+		oBrowser.manage().window().maximize();
+	}catch (Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+public static void navigate()
+{
+	try 
+	{
+		oBrowser.get("http://localhost:81/login.do");
+	}catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+static void login()
+{
+	try
+	{
+		oBrowser.findElement(By.id("username")).sendKeys("admin");
+		oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+		oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+		Thread.sleep(5000);
+	}catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+static void miniPopwindow()
+{
+	try
+	{
+		oBrowser.findElement(By.id("gettingStartedShortcutsMenuCloseId")).click();
+		Thread.sleep(5000);
+		
+	}catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+ public static void createUser()
+ {
+	 try
+	 {
+		 oBrowser.findElement(By.xpath("//*[@id=\'topnav\']/tbody/tr[1]/td[5]/a")).click();
+		 Thread.sleep(2000);
+		 oBrowser.findElement(By.xpath("//*[@id=\"createUserDiv\"]/div")).click();
+		 Thread.sleep(2000);
+		 oBrowser.findElement(By.name("firstName")).sendKeys("user");
+		 Thread.sleep(2000);
+		 oBrowser.findElement(By.id("userDataLightBox_lastNameField")).sendKeys("1");
+		 Thread.sleep(2000);
+		 oBrowser.findElement(By.name("email")).sendKeys("user1@gmail.com");
+		 Thread.sleep(2000);
+		 oBrowser.findElement(By.name("username")).sendKeys("user1");
+		 Thread.sleep(2000);
+		 oBrowser.findElement(By.name("password")).sendKeys("1234");
+		 Thread.sleep(2000);
+		 oBrowser.findElement(By.name("passwordCopy")).sendKeys("1234");
+		 Thread.sleep(2000);
+	     oBrowser.findElement(By.xpath("//*[@id=\'userDataLightBox_commitBtn\']/div/span")).click();
+	     Thread.sleep(2000);
+		 
+	 }catch(Exception e)
+	 {
+		 e.printStackTrace();
+	 }
+ }
+ public static void modifyUser() 
+ {
+	 try
+	 {
+//		 oBrowser.findElements(By.xpath("//*[@id=\'userListTableContainer\']/table/tbody/tr[2]/td[4]")).click();
+//		 oBrowser.findElements(By.xpath("//*[@id=\'ext-gen1108\']")).c
+//		 
+		 
+	 }catch(Exception e)
+	 {
+		 e.printStackTrace();
+	 }
+ }
+public static void deleteUser()
+{
+	try
+	{
+		oBrowser.findElement(By.xpath("//*[@id='userListTableContainer']/table/tbody/tr[2]/td[1]/table/tbody/tr/td/div[1]/span[2]")).click();
+		Thread.sleep(2000);
+		oBrowser.findElement(By.id("userDataLightBox_deleteBtn")).click();
+		Thread.sleep(2000);
+		oBrowser.switchTo().alert().accept();
+		Thread.sleep(2000);
+	}catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+static void logout()
+{
+	try
+	{
+		oBrowser.findElement(By.linkText("Logout")).click();
+		Thread.sleep(2000);
+	}catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+
+static void closeApplication()
+{
+	try
+	{
+		oBrowser.close();
+	}catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+}
+ }
+
